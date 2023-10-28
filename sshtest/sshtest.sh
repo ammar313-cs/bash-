05b7897a6c14d9c7c336f9c13b7d869f7352e350
System Administrator <ammarfarrukh2017@gmail.com>
2023-10-28 16:55:17 +0500

    2nd commit

--- sshtest/sshtest.sh
+++ sshtest/sshtest.sh
@@ -8,10 +8,7 @@ github_repo="your-repo name"
 ssh_key_path="~/.ssh/id_rsa"
 
 
-ssh_key_passphrase=""
-
-
-ssh-keygen -t rsa -C "your_email@example.com should be given here "
+ssh-keygen -t rsa -C "your_email@example.com should be given here"
 
 
 ssh-add $ssh_key_path
@@ -24,13 +21,5 @@ ssh_config="Host github.com
 
 echo "$ssh_config" > ~/.ssh/config
 
-
-pub_key=$(cat $ssh_key_path.pub)
-
-curl -u "$github_username" -H "Content-Type: application/json" -X POST -d "{\"title\":\"SSH Key\",\"key\":\"$pub_key\"}" https://api.github.com/user/keys
-
-
-git remote set-url origin "git@github.com:$github_username/$github_repo.git"
-
-
 echo "SSH key generation and setup for GitHub completed!"
+
